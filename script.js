1c7e819471dae3d65fef26732a03f8cdf41ebd53
ivanlalic <ivanlalic@gmail.com>
2023-10-17 23:28:13 -0300

    Es-US language selector added

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const phoneInput = document.getElementById("phone");
     const messageInput = document.getElementById("message");
     const sendButton = document.getElementById("send-button");
+    const headerLogo = document.querySelector(".logo");
+    const mainTitle = document.querySelector("h1");
+    const footerText = document.querySelector("footer");
 
     sendButton.addEventListener("click", function () {
         const phoneNumber = phoneInput.value;
@@ -15,4 +18,40 @@ document.addEventListener("DOMContentLoaded", function () {
             window.location.href = whatsappURL;
         }
     });
+
+    // Language selection functionality
+    const usFlag = document.getElementById("us-flag");
+    const esFlag = document.getElementById("es-flag");
+
+    usFlag.addEventListener("click", function (event) {
+        event.preventDefault();
+        setLanguage("en");
+    });
+
+    esFlag.addEventListener("click", function (event) {
+        event.preventDefault();
+        setLanguage("es");
+    });
+
+    function setLanguage(lang) {
+        // Implement language-specific content changes
+        if (lang === "es") {
+            headerLogo.textContent = "WhatsApp";
+            mainTitle.textContent = "Envia Mensajes de WhatsApp Sin Agregar Contacto";
+            footerText.textContent = "2023 WhatsApp Message Sender - No guardamos ninguna información personal";
+            // Update input placeholders and button text for Spanish version
+            phoneInput.placeholder = "CodigoPaisNumero";
+            messageInput.placeholder = "Tu Mensaje";
+            sendButton.textContent = "Enviar via WhatsApp";
+        } else {
+            // Default to English
+            headerLogo.textContent = "WhatsApp";
+            mainTitle.textContent = "Send a WhatsApp Message Without Adding the Contact";
+            footerText.textContent = "2023 WhatsApp Message Sender - We don't store any personal information";
+            // Reset input placeholders and button text to English version
+            phoneInput.placeholder = "CountryCodePhone";
+            messageInput.placeholder = "Your message";
+            sendButton.textContent = "Send via WhatsApp";
+        }
+    }
 });
